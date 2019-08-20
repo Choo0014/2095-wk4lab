d7dc17fdeb1cbb4142852e1d9e896bbbfa0aa99c
Chun Yin Ho Wesley <choo0014@student.monash.edu>
2019-08-20 14:16:30 +1000

    Working input and list all

--- router.js
+++ router.js
@@ -5,7 +5,19 @@ router.get('/', function(req, res){
    res.send('FIT2095 Week 4 lab');
 });
 
+router.get('/addItem/:name/:quantity/:price',function(req,res){
+    console.log(req.params.name); //to access the name
+    console.log(req.params.quantity); //to access the quantity
+    console.log(req.params.price); //to access the price
+    let newId = getNewRandomId();
+    let obj = {id:newId,itemName:name,itemQuantity:parseInt(req.params.quantity),itemPrice:parseInt(req.params.price)};
+    console.log(obj);
+    db.push(obj);
+    res.send("Your input has been save to the database.");
+    console.log(db);
 
+
+});
 
 router.get('/about', function(req, res){
    res.send('This page is about FIT2095');
--- week4lab.js
+++ week4lab.js
@@ -1,9 +1,9 @@
 let express = require('Express');
 let app = express();
-let router = require('./router.js');
+//let router = require('./router.js');
 
 let db = [];
-let newId= Math.round(Math.random()*1000)
+let newId = Math.round(Math.random() * 1000)
 
 let rec = {
     id: getNewRandomId(),
@@ -13,12 +13,27 @@ let rec = {
 };
 
 
-app.get('/newItem/:name/:quantity/:price',function(req,res){
-    console.log(req.params.name); //to access the name
-    console.log(req.params.quantity); //to access the quantity
-    console.log(req.params.price); //to access the price
+app.get('/', function (req, res) {
+    res.send('FIT2095 Week 4 lab');
+});
+
+app.get('/newItem/:name/:quantity/:price', function (req, res) {
+
+
     let newId = getNewRandomId();
-    let obj = {id:newId,itemName:name,itemQuantity:parseInt(req.params.quantity),itemPrice:parseInt(req.params.price)};
+    let newName = req.params.name;
+    let newQuantity = parseInt(req.params.quantity);
+    let newPrice = parseInt(req.params.price);
+    let obj = {
+        itemId: newId,
+        itemName: newName,
+        itemQuantity: newQuantity,
+        itemPrice: newPrice
+    };
+    console.log(req.params);
+    // console.log(req.params.name); //to access the name
+    // console.log(req.params.quantity); //to access the quantity
+    // console.log(req.params.price); //to access the price
     console.log(obj);
     db.push(obj);
     res.send("Your input has been save to the database.");
@@ -27,13 +42,30 @@ app.get('/newItem/:name/:quantity/:price',function(req,res){
 
 });
 
-function getNewRandomId(){
+app.get('/listAll', function (req, res) {
+    let table = generateList();
+    res.send(table);
+}); //if an app gets this pathname it will respond with this response
+
+
+
+function generateList() {
+    let st = "Index" + "| " + "ID" + " | " + "Name" + " | " + "Quantity" + " | " + "Price" + " | " + "Cost" + "</br>" + "</br>";
+    for (let i = 0; i < db.length; i++) {
+        let itemCost = db[i].itemQuantity *db[i].itemPrice;
+        st += (i) + " - " + db[i].itemId + " | " + db[i].itemName + "|" + db[i].itemQuantity + "|" + db[i].itemPrice + "|" + itemCost + "</br>";
+
+    }
+    return st;
+}
+
+function getNewRandomId() {
     let id;
-    id = Math.round(Math.random()*1000); //gives between 0 and 1, multiply by 1000. rounds to nearest integer
+    id = Math.round(Math.random() * 1000); //gives between 0 and 1, multiply by 1000. rounds to nearest integer
     return id;
 
 }
 
-app.use('/', router);
+//app.use('/', router);
 
 app.listen(8080);
