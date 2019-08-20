e98b4fb9d1eefbac1a17615c90a2a399bf8129ea
Chun Yin Ho Wesley <choo0014@student.monash.edu>
2019-08-20 13:20:14 +1000

    Taking input 1

--- router.js
+++ router.js
@@ -2,8 +2,10 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res){
-   res.send('Welcome to FIT2095 Home Page');
+   res.send('FIT2095 Week 4 lab');
 });
+
+
 
 router.get('/about', function(req, res){
    res.send('This page is about FIT2095');
--- week4lab.js
+++ week4lab.js
@@ -6,11 +6,33 @@ let db = [];
 let newId= Math.round(Math.random()*1000)
 
 let rec = {
-    id: newId,
+    id: getNewRandomId(),
     name: 'Tim',
     quantity: 23,
     price: 'Mel'
 };
+
+
+app.get('/newItem/:name/:quantity/:price',function(req,res){
+    console.log(req.params.name); //to access the name
+    console.log(req.params.quantity); //to access the quantity
+    console.log(req.params.price); //to access the price
+    let newId = getNewRandomId();
+    let obj = {id:newId,itemName:name,itemQuantity:parseInt(req.params.quantity),itemPrice:parseInt(req.params.price)};
+    console.log(obj);
+    db.push(obj);
+    res.send("Your input has been save to the database.");
+    console.log(db);
+
+
+});
+
+function getNewRandomId(){
+    let id;
+    id = Math.round(Math.random()*1000); //gives between 0 and 1, multiply by 1000. rounds to nearest integer
+    return id;
+
+}
 
 app.use('/', router);
 
