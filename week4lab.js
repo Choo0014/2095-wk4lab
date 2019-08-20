3f5098eab4d0cec558503a90d2ff0c187c900b19
Chun Yin Ho Wesley <choo0014@student.monash.edu>
2019-08-20 16:48:30 +1000

    Ver 2

--- week4lab.js
+++ week4lab.js
@@ -3,33 +3,26 @@ let app = express();
 //let router = require('./router.js');
 
 let db = [];
-let newId = Math.round(Math.random() * 1000)
-
-let rec = {
-    id: getNewRandomId(),
-    name: 'Tim',
-    quantity: 23,
-    price: 'Mel'
-};
 
 
 app.get('/', function (req, res) {
     res.send('FIT2095 Week 4 lab');
 });
 
-app.get('/newItem/:name/:quantity/:price', function (req, res) {
-
+app.get('/newItem/:name/:quantity/:price', function (req, res) { //user input their items
 
     let newId = getNewRandomId();
     let newName = req.params.name;
     let newQuantity = parseInt(req.params.quantity);
     let newPrice = parseInt(req.params.price);
+
     let obj = {
         itemId: newId,
         itemName: newName,
         itemQuantity: newQuantity,
         itemPrice: newPrice
     };
+
     console.log(req.params);
     // console.log(req.params.name); //to access the name
     // console.log(req.params.quantity); //to access the quantity
@@ -39,16 +32,14 @@ app.get('/newItem/:name/:quantity/:price', function (req, res) {
     res.send("Your input has been save to the database.");
     console.log(db);
 
-
 });
 
-app.get('/listAll', function (req, res) {
-    let table = generateList();
-    res.send(table);
-}); //if an app gets this pathname it will respond with this response
+app.get('/listAll', function (req, res) { //listing all the items in the database
+    res.send(generateList());
+}); 
 
 app.get('/deleteItem/:inputItemId', function (req, res) { // /: represents a variable - a parameter
-    //if a request arrives that matches this statement, execute this:
+
     let search = false;
     let msg = "";
     let trashId = parseInt(req.params.inputItemId);
@@ -57,9 +48,10 @@ app.get('/deleteItem/:inputItemId', function (req, res) { // /: represents a var
 
     for (let i = 0; i < db.length && !search; i++) { //iterates between 0 and no found. Once found, found becomes TRUE
         if (db[i].itemId === trashId) {
-            db = db.splice(i, 1);
+            db.splice(i, 1);                            //db =.... (big mistake)
             search = true;
             msg = "Item: " + trashId + " has been deleted."
+            console.log(db);
         }
 
     }
@@ -75,18 +67,19 @@ app.get('/deleteItem/:inputItemId', function (req, res) { // /: represents a var
 
 app.get('/totalValue', (req, res) => {
     let value = 0;
-    db.forEach(element => {
-        value += parseInt(element.quantity * element.price);
+    // db.forEach(element => {
+    //     value += parseInt(element.quantity * element.price);
 
-    })
-    res.send('Warehouse total value: ' + value);
+    // })
+    // res.send('Warehouse total value: ' + value);
+
 })
 
 
 
-function deleteItem(itemId) {
-    db.splice(itemId, 1);
-}
+// function deleteItem(itemId) {
+//     db.splice(itemId, 1);
+// }
 
 
 function getNewRandomId() {
@@ -100,7 +93,7 @@ function generateList() {
     let st = "Index" + "| " + "ID" + " | " + "Name" + " | " + "Quantity" + " | " + "Price" + " | " + "Cost" + "</br>" + "</br>";
     for (let i = 0; i < db.length; i++) {
         let itemCost = db[i].itemQuantity * db[i].itemPrice;
-        st += (i) + " - " + db[i].itemId + " | " + db[i].itemName + "|" + db[i].itemQuantity + "|" + db[i].itemPrice + "|" + itemCost + "</br>";
+        st += db[i].itemId + " | " + db[i].itemName + "|" + db[i].itemQuantity + "|" + db[i].itemPrice + "|" + itemCost + "</br>";
 
     }
     return st;
